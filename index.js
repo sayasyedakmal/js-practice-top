e7d66ade970c51831f1e98e66694a01a9e6bdb3e
Syed Akmal <sayasyedakmal@gmail.com>
2022-11-02 22:03:04 +0800

    Completed exercise for Fundamentals Part 1
    - practicing using console.log
    - practicing doing simple math
    
    Syed Akmal
    Full-Stack Web Developer

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("HELLO WORLD");
+console.log("SCRIPT START....");
+
+/*
+let admin
+let name = "John";
+
+admin = name;
+
+alert(admin);
+
+*/
+
+// let ourPlatnetName = "Earth";
+// let currentUserName = "John";
+
+//---------------------------------------
+//https://www.theodinproject.com/lessons/foundations-fundamentals-part-1
+console.log(24 + 2); // #1
+
+
+console.log(1 + 2 + 3 + 4 + 5 + 6); // #2
+
+console.log((4 + 6 + 9) / 77); // #3
+
+//#4
+let a = 10;
+console.log(a);
+
+console.log(9 * a);
+
+let b = 7 * a;
+
+console.log(b);
+
+//#5
+console.log('#5');
+
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+
